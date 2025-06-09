eef172c3f34aae35f3e226679fcb7d25b8646bcc
Swarup Paul <swarup.r.paul@globallogic.com>
2025-06-09 11:35:05 +0530

    updated code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, DevOps World!"
+    return "Hello, Welcome to my DevOps Deployment Demo!"
 
 if __name__ == '__main__':
     app.run(debug=True)
