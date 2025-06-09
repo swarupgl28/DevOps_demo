4a25cc3cbf278379297afa589bf23233c437775c
Swarup Paul <swarup.r.paul@globallogic.com>
2025-06-09 11:42:26 +0530

    4th updated code

--- testapp.py
+++ testapp.py
@@ -4,7 +4,7 @@ def test_home():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
-    assert b"Hello, DevOps World!" in response.data
+    assert b"Hello, Welcome to my DevOps Deployment Demo!" in response.data
 
     
 
